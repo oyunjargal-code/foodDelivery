35cc1fc4a7b51910e5157577467adc294ecf2d6c
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-03-18 12:35:03 +0800

    03.18 angid

--- src/controller/orders/add-order.ts
+++ src/controller/orders/add-order.ts
@@ -1,24 +1,64 @@
 import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
 
+type OrderItems = {
+  foodId: number;
+  quantity: number;
+};
+
+type BodyType = {
+  userId: number;
+  orderItems: OrderItems[];
+};
+
 export const addOrder = async (req: Request, res: Response) => {
+  const { userId, orderItems }: BodyType = req.body;
+
+  const totalPrice = await calcTotalPrice(orderItems);
+
   try {
     const order = await prisma.foodOrder.create({
       data: {
+        userId,
         status: "pending",
-        totalPrice: "100",
+        totalPrice: totalPrice,
+        foodOrderItems: {
+          create: orderItems,
+        },
       },
     });
-
-    const orderItem = await prisma.foodOrderItems.create({
-      data: {
-        foodId: 1,
-        quantity: 2,
-        foodOrderId: order.id,
-      },
-    });
-    res.json({ order, orderItem });
+    res.json({ order });
   } catch (error) {
     res.status(500).json(error);
   }
+};
+
+const calcTotalPrice = async (orderItems: OrderItems[]) => {
+  const foodIds = orderItems.map((orderItem) => orderItem.foodId);
+
+  const foods = await findFoodsByIds(foodIds);
+
+  const foodWithQuantity = foods.map((food) => {
+    const foundedOrderItem = orderItems.find(
+      (orderItem) => orderItem.foodId === food.id,
+    );
+    return { ...food, quantity: foundedOrderItem?.quantity };
+  });
+
+  const totalPrice = foodWithQuantity.reduce((acc, curr) => {
+    return acc + Number(curr.price) * Number(curr.quantity);
+  }, 0);
+
+  return totalPrice.toString();
+};
+
+const findFoodsByIds = async (foodIds: number[]) => {
+  const foods = await prisma.food.findMany({
+    where: {
+      id: {
+        in: foodIds,
+      },
+    },
+  });
+  return foods;
 };
