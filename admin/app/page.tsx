10f94a47a6344f5d0fbdbd3ef6133353a1779f73
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-03-23 14:55:14 +0800

    03.23 angid

---
+++ admin/app/page.tsx
@@ -0,0 +1,82 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarProvider,
+} from "@/components/ui/sidebar";
+import { LayoutDashboard, Van } from "lucide-react";
+
+export default function Home() {
+  const categories = [
+    { label: "All Dishes", count: 112 },
+    { label: "Appetizers", count: 6 },
+    { label: "Salads", count: 3 },
+    { label: "Pizzas", count: 5 },
+    { label: "Lunch favorites", count: 6 },
+    { label: "Main dishes", count: 5 },
+    { label: "Fish & Sea foods", count: 5 },
+    { label: "Brunch", count: 5 },
+    { label: "Side dish ", count: 5 },
+    { label: "Desserts", count: 5 },
+    { label: "Beverages", count: 5 },
+  ];
+
+  return (
+    <div className="flex gap-4">
+      <div>
+        <SidebarProvider className="bg-[#FFFFFF]">
+          <Sidebar style={{ backgroundColor: "white" }}>
+            <SidebarHeader>
+              <div className="flex gap-2">
+                <img className="w-10 h-10 m-2" src="/nomnom_logo.svg" alt="" />
+                <div className="font-serif">
+                  <h1 className="font-semibold">NomNom</h1>
+                  <h4>Swift delivery</h4>
+                </div>
+              </div>
+            </SidebarHeader>
+            <SidebarContent>
+              <SidebarGroup></SidebarGroup>
+
+              <SidebarGroup>
+                <div className="flex justify-center">
+                  <Button className="w-40 h-10 rounded-2xl text-[14px] font-serif">
+                    <LayoutDashboard />
+                    Food menu
+                  </Button>
+                </div>
+              </SidebarGroup>
+              <SidebarGroup>
+                <div className="flex justify-center">
+                  <Button
+                    variant="ghost"
+                    className="w-40 h-10 rounded-2xl text-[14px] font-serif"
+                  >
+                    <Van />
+                    Orders
+                  </Button>
+                </div>
+              </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>Hul</SidebarFooter>
+          </Sidebar>
+        </SidebarProvider>
+      </div>
+      <div>
+        <h1>Dishes category</h1>
+        <div className="flex gap-2">
+          {categories.map((category) => (
+            <Button key={category.label} variant="outline">
+              {category.label}
+              <Badge variant="default">{category.count}</Badge>
+            </Button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
