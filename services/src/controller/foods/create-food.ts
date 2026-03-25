a4905196c5792f7c96998235968d52724e42d981
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-03-25 20:13:59 +0800

    category

--- services/src/controller/foods/create-food.ts
+++ services/src/controller/foods/create-food.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import { prisma } from "../../lib/prisma";
 
 export const createFood = async (req: Request, res: Response) => {
-  const { name, price, foodCategoyId } = req.body;
+  const { name, price, foodCategoyId, ingredients } = req.body;
 
   try {
     const food = await prisma.food.create({
-      data: { name, price, foodCategoyId: Number(foodCategoyId) },
+      data: { name, price, ingredients, foodCategoyId: Number(foodCategoyId) },
     });
     res.status(200).json([food]);
   } catch (error) {
