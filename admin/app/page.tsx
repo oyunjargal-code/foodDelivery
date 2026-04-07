60ef4f1678da1de1b1e5cedda44a51eb6b3329f4
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-04-07 15:44:38 +0800

    singIn url

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -35,13 +35,16 @@ const SignIn = () => {
     };
 
     try {
-      const res = await fetch("http://localhost:3001/users/auth", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/auth"`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
         },
-        body: JSON.stringify(credentials),
-      });
+      );
 
       if (res.status !== 200) return;
 
